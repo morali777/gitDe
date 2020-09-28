75534ed0894cb4d716e61eaf2dc231e854b92301
morali777 <nesemorali@yahoo.com>
2020-09-28 07:24:05 -0400

    updated

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,7 +1,8 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is Sdet");
-
+        System.out.println("future Sdet");
+        System.out.println("qa ");
 
     }
 }
