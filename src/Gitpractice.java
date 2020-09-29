5dfa1a4bb016f37abf6d0dd94feea6de4452803a
morali777 <nesemorali@yahoo.com>
2020-09-29 07:46:30 -0400

    bbokAroom.java

--- src/Gitpractice.java
+++ src/Gitpractice.java
@@ -1,7 +1,7 @@
 public class Gitpractice {
     public static void main(String[] args) {
         System.out.println("JP Morgan");
-        System.out.println("Sdet");
+        System.out.println("qa");
 
 
     }
